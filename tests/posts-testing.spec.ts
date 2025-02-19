134fbe193b802a9156c32b77756c373c6568218d
Sujith Quintelier <sujith.quintelier@motors.co.uk>
2025-02-19 16:37:16 +0100

    Add gray-matter and glob dependencies; implement Playwright tests for post snapshots

---
+++ tests/posts-testing.spec.ts
@@ -0,0 +1,36 @@
+import { test, expect } from '@playwright/test'
+
+import matter from 'gray-matter'
+import * as fs from 'node:fs'
+import { globSync } from 'glob'
+import path from 'node:path'
+
+let files = globSync(['content/posts/**/*.md'])
+
+let paths = files.flatMap(file => {
+  // Some posts are content/posts/{slug}/index.md
+  // others are content/posts/{slug}.md
+  // Extract {slug}.
+  let slug = (path.basename(file) === 'index.md')
+    ? path.dirname(file).slice("content/".length)
+    : `posts/${path.basename(file, '.md')}`
+
+  let frontmatter = matter(fs.readFileSync(file))
+  return frontmatter.data.draft ? [] : [slug]
+})
+
+test.describe.configure({ mode: 'parallel' })
+
+for (const path of paths) {
+  test(path, async ({ page }) => {
+    await page.goto(path)
+
+    await expect(page).toHaveScreenshot({
+      fullPage: true,
+      // Apply mask over all images.
+      // Some images, like GIFs, can change between snapshots
+      // and lead to flaky tests.
+      // mask: await page.getByRole('img').all(),
+    })
+  })
+}
